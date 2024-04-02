6e2f72670d2ce070905a40fef2bd4800d7c28f07
Sonic <tuenx2611@gmail.com>
2024-04-03 05:15:54 +0700

    fix api

--- src/app/main/meal-plan/meal-plan-detail/type/MenuType.tsx
+++ src/app/main/meal-plan/meal-plan-detail/type/MenuType.tsx
@@ -1,4 +1,22 @@
-export type NameType ={
-    id:string ;
-    name:string;
+import { FoodType } from "../../calendar/types/PlanType";
+
+export type NameType = {
+    id: string;
+    name: string;
 }
+export type menuSampleType = {
+    menuMealSamples: menuMealSampleType[]
+}
+export type menuMealSampleType = {
+    from: string;
+    to: string;
+    mealItemSamples: mealItemSamplesType[];
+    name: string;
+
+}
+export type mealItemSamplesType = {
+    quantity: number;
+    food: FoodType;
+    order: number;
+}
+
